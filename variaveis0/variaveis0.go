c104964a405b6ff676c4d687298310357b15c764
LuanaCajaiba <luana_cajaiba1@hotmail.com>
2021-03-18 19:32:29 -0300

    atualização

--- variaveis0/variaveis0.go
+++ variaveis0/variaveis0.go
@@ -10,6 +10,8 @@ type hotdog int
 // foi criada uma variável para esse tipo, com o identificador x, utilizando a palavra-chave var
 var x hotdog
 
+var y int
+
 func main() {
 	//foi demonstrada o valor da variável x
 	fmt.Println(x)
@@ -19,5 +21,11 @@ func main() {
 	x = 42
 	//foi demonstrado o valor da variável x
 	fmt.Println(x)
+	//foi utilizada a conversão para transformar o tipo do valor da variável "x" em seu tipo subjacente
+	y = int(x)
+	// foi demonstrado o valor de y
+	fmt.Println(y)
+	// foi demonstrado o tipo de y
+	fmt.Printf("%T\n", y)
 
 }
