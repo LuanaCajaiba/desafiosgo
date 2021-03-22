aeacdd0c40e0e750b833c29a31dd4da1b7e966aa
LuanaCajaiba <luana_cajaiba1@hotmail.com>
2021-03-22 18:10:21 -0300

    Tipo String atualização0

--- Tipo_String/tipostring.go
+++ Tipo_String/tipostring.go
@@ -8,4 +8,13 @@ func main() {
 	s := "Hello Luana"
 	fmt.Printf("%v \n %T", s, s)
 
+	j := `  colando
+	
+	aspas             simples
+	
+	tem como          colocar 
+	
+	bagunçado`
+	fmt.Printf("%v \n  %T", j, j)
+
 }
