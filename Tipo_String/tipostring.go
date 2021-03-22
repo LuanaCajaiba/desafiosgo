ee81ac2ee00efd2b4f0b766a43557ce8fdd4fbf3
LuanaCajaiba <luana_cajaiba1@hotmail.com>
2021-03-22 18:50:41 -0300

    Tipo String atualização4

--- Tipo_String/tipostring.go
+++ Tipo_String/tipostring.go
@@ -11,7 +11,23 @@ func main() {
 	for _, v := range sb {
 
 		fmt.Printf("%v - %T - %#U - %#x\n", v, v, v, v)
+		//v - valor numérico
+		//t - tipo
+		//unicode
+		//tipo hexadecimal
 	}
+
+	fmt.Println(" ")
+
+	for _, v := range s {
+		fmt.Printf("%v - %T - %#U - %#x\n", v, v, v, v)
+	}
+
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("%v - %T - %#U - %#x\n", s[i], s[i], s[i], s[i])
+	}
+
+	fmt.Println(" ")
 
 	j := `  colando
 	
