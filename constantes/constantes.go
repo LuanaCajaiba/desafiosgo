4b0327c40f26c5f2f22fa89489bf5699dd8f6179
LuanaCajaiba <luana_cajaiba1@hotmail.com>
2021-03-23 15:34:00 -0300

    Constantes1

--- constantes/constantes.go
+++ constantes/constantes.go
@@ -7,7 +7,6 @@ import (
 const x = 10 //ficará indefinido até ser usado (definido no momento do uso)
 //var y = 10 //no momento que o programa rodar, será decidido que é um int (definido no momento da atribuição)
 //var c int
-
 var d float64
 
 func main() {
