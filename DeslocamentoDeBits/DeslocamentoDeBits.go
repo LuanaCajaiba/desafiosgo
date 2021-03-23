3118b0c3a92b717168f5961eb302b1b3a4807c47
LuanaCajaiba <luana_cajaiba1@hotmail.com>
2021-03-23 17:17:34 -0300

    Deslocamento de Bits1

--- DeslocamentoDeBits/DeslocamentoDeBits.go
+++ DeslocamentoDeBits/DeslocamentoDeBits.go
@@ -7,7 +7,7 @@ import (
 func main() {
 
 	x := 1
-	y := x << 1
+	y := x << 1 //operador deslocamento de bits
 
 	fmt.Printf("%b\n", x)
 	fmt.Printf("%b\n", y)
---
+++ DeslocamentoDeBits/DeslocamentoDeBits1.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	y := 24
+
+	x := y << 2
+	z := y >> 2
+
+	fmt.Printf("%b\n", y)
+	fmt.Printf("%b\n", x)
+	fmt.Printf("%b\n", z)
+
+}
