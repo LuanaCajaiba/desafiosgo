8282b4b5875caa8cb66388f4184833db96fe412d
LuanaCajaiba <luana_cajaiba1@hotmail.com>
2021-03-24 01:09:14 -0300

    Iota1 - Atualização

--- aprendendoIota/aprendendoIota1.go
+++ aprendendoIota/aprendendoIota1.go
@@ -10,9 +10,25 @@ const (
 	_ //usado para pular o número
 	c
 	d
+
+	const (
+		_  = iota             // 0
+		KB = 1 << (iota * 10) // 1 << (1 * 10)
+		MB = 1 << (iota * 10) // 1 << (2 * 10)
+		GB = 1 << (iota * 10) // 1 << (3 * 10)
+		TB = 1 << (iota * 10) // 1 << (4 * 10)
+	)
 )
 
 func main() {
 	fmt.Println(a, b, c, d)
+
+	fmt.Println("binary\t\t\t\tdecimal")
+	fmt.Printf("%b\t\t\t", KB)
+	fmt.Printf("%d\n", KB)
+	fmt.Printf("%b\t\t", MB)
+	fmt.Printf("%d\n", MB)
+	fmt.Printf("%b\t", GB)
+	fmt.Printf("%d\n", GB)
 
 }
