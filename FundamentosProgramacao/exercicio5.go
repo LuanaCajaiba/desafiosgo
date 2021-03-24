eef4851d6e1b98fa671d9ecb96265db20c2120db
LuanaCajaiba <luana_cajaiba1@hotmail.com>
2021-03-24 00:44:39 -0300

    Exercício5 - nível 2

---
+++ FundamentosProgramacao/exercicio5.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := ` com raw 
+	
+	string
+	
+	   literal
+	   
+	             dá 
+				 
+				 pra fazer uma 
+				 
+				 bagunça na tela`
+
+	fmt.Println(x)
+}
