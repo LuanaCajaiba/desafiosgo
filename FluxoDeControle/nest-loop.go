981624dc844c84fe166c48e951aec2eb05c4810d
LuanaCajaiba <luana_cajaiba1@hotmail.com>
2021-03-28 02:35:20 -0300

    Repetição hierárquica - Atualização

--- FluxoDeControle/nest-loop.go
+++ FluxoDeControle/nest-loop.go
@@ -4,12 +4,20 @@ import "fmt"
 
 //repetição hierárquica
 func main() {
-	for hora := 0; hora <= 12; hora++ {
+	/*for hora := 0; hora <= 12; hora++ {
 		fmt.Println("h: ", hora)
 
 		for minuto := 0; minuto < 60; minuto++ { //esse loop será repetido inteiro a cada for rodado em cima
 			fmt.Print(" ", minuto)
 		}
 		fmt.Println("\n")
+	}*/
+
+	for mes := 1; mes <= 12; mes++ {
+		fmt.Println("Mês", mes)
+		for dias := 1; dias <= 30; dias++ {
+			fmt.Print("Dia: ", dias, ", ")
+		}
 	}
+	fmt.Println("\n")
 }
