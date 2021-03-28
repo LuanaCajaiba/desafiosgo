da09f6daabfb2c9c1557dd5c8897eb110fe9ae99
LuanaCajaiba <luana_cajaiba1@hotmail.com>
2021-03-28 01:27:27 -0300

    Fluxo de Controle - For

---
+++ FluxoDeControle/for.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	for x := 0; x < 10; x++ { //for x é igual a 0, enquanto x for menor que 10 acrescenta-se 1 ao x
+		fmt.Println(x)
+	}
+}
+
+//inicialização x:=0
+//condição  x < 10
+//pós x++
+
+//na linguagem go ao final de toda instrução existe um ponto e vírgula, se não posto pelo usuário é posto pelo compilador
