781a10cd097ff0881eda8a7a751f1271ec8bda49
LuanaCajaiba <luana_cajaiba1@hotmail.com>
2021-03-28 18:38:31 -0300

    If

--- FluxoDeControle/if.go
+++ FluxoDeControle/if.go
@@ -5,6 +5,19 @@ import "fmt"
 func main() {
 	x := 10
 	if x < 100 {
-		fmt.Println("coe")
+		fmt.Println("x é menor que 100")
 	}
+
+	if x < 100 {
+		fmt.Println("x é menor que 100, só que a condição está entre parenteses")
+	}
+
+	if !(x < 100) {
+		fmt.Println("x não é menor que 100")
+	}
+
+	if !(x > 100) {
+		fmt.Println("x não é maior que 100")
+	}
+
 }
