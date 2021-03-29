0c353d8cbda862e843e0a8899dd2a42e6fb05173
LuanaCajaiba <luana_cajaiba1@hotmail.com>
2021-03-29 14:36:17 -0300

    Atualização

---
+++ codeacademy/teste.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Define daysOnVacation using := below:
+
+	daysOnVacation := 5
+
+	// Define hoursInDay using var and = below:
+	var hoursInday int = 24
+
+	fmt.Println("You have spent", daysOnVacation*hoursInday, "hours on vacation.")
+}
