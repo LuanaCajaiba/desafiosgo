17889bb93edb16bc151fda9d1110a0bddf769005
LuanaCajaiba <luana_cajaiba1@hotmail.com>
2021-03-30 02:00:56 -0300

    Atualização

--- FluxoDeControle/switch.go
+++ FluxoDeControle/switch.go
@@ -18,4 +18,22 @@ func main() {
 
 	}
 
+	quemtanoescritorio := "Luana"
+
+	switch quemtanoescritorio {
+	case "Luiz":
+		fmt.Println("Quem tá no escritório é o Luiz ")
+	case "Joana":
+		fmt.Println("Quem tá no escritório é a Joana ")
+	case "Maria":
+		fmt.Println("Quem tá no escritório é a Maria ")
+	case "Luana":
+		fmt.Println("Quem tá no escritório é a Luana ")
+
+	}
+
 }
+
+//switch avalia o código de cima para baixo.
+//se houver uma expressão correta na 1ºexpressão
+//ele retornará apenas a 1ºexpressão
