b60bc6d78b915cf9f4387ccc9839892f57ed847e
LuanaCajaiba <luana_cajaiba1@hotmail.com>
2021-04-03 21:56:03 -0300

    Atualização

--- FluxoDeControle/operadores-logicos-condicionais.go
+++ FluxoDeControle/operadores-logicos-condicionais.go
@@ -3,9 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	x := 3
+	x := 6
 
 	if (x == 2) || (x == 3) {
 		fmt.Println("X é dois ou três")
+	}
+
+	if x%2 == 0 && x%3 == 0 {
+		fmt.Println("é multiplo de dois e também de três")
 	}
 }
