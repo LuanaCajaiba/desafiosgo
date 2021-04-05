9107286e1497ded20f70d5fb6161d6e6bdb7b6e9
LuanaCajaiba <luana_cajaiba1@hotmail.com>
2021-04-05 00:06:50 -0300

    Atualização

---
+++ FluxoDeControle/exercicio5.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	x := 0
+	if x == 1 {
+		fmt.Println("Zero é igual a um")
+	} else if x > 1 {
+		fmt.Println("Zero é maior que 1 ")
+	} else {
+		fmt.Println("Zero não é nem igual, nem maior que 1")
+	}
+
+}
