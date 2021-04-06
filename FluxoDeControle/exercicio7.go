37a574196f9255e54270e48469f08c61a5adcb3d
LuanaCajaiba <luana_cajaiba1@hotmail.com>
2021-04-06 17:06:00 -0300

    Atualização

---
+++ FluxoDeControle/exercicio7.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	esportefavorito := "vôlei"
+
+	switch esportefavorito {
+	case "futebol":
+		fmt.Println("É futebol")
+	case "vôlei":
+		fmt.Println("É vôlei")
+	case "botão":
+		fmt.Println("É botão")
+
+	}
+
+}
