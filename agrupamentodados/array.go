539757a182acd1380a5d12989d7b6dc0c6bd3dc7
LuanaCajaiba <luana_cajaiba1@hotmail.com>
2021-04-06 21:27:57 -0300

    Atualização

--- agrupamentodados/array.go
+++ agrupamentodados/array.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 var x [5]int
+var y [6]int
 
 func main() {
 
@@ -10,5 +11,8 @@ func main() {
 	x[1] = 10
 	fmt.Println(x[0], x[1])
 	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+	fmt.Printf("%T\n", y)
+	fmt.Println(len(x))
 
 }
---
+++ agrupamentodados/slice.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	array := [5]int{1, 2, 3, 4, 5}
+	fmt.Println(array)
+
+	slice := []int{1, 2, 3, 4, 5}
+	fmt.Println(slice)
+
+}
