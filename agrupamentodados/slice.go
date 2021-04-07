a9e975a682c865e01f2d7aa5f079f37e76c50f20
LuanaCajaiba <luana_cajaiba1@hotmail.com>
2021-04-06 21:39:30 -0300

    Atualização

--- agrupamentodados/slice.go
+++ agrupamentodados/slice.go
@@ -3,10 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	array := [5]int{1, 2, 3, 4, 5}
+	array := [5]int{1, 2, 3, 4, 5} //número finito especificado
 	fmt.Println(array)
 
-	slice := []int{1, 2, 3, 4, 5}
+	slice := []int{1, 2, 3, 4, 5} //número não especificado
 	fmt.Println(slice)
+
+	slice2 := append(slice, 6)
+
+	fmt.Println(slice2)
 
 }
