6ca308a4254aaff026097bbe9c2c91b190dd6bb0
LuanaCajaiba <luana_cajaiba1@hotmail.com>
2021-04-10 19:07:11 -0300

    Atualização

--- agrupamentodados/exercicio.go
+++ agrupamentodados/exercicio.go
@@ -8,8 +8,8 @@ func main() {
 
 	array := [5]int{1, 2, 3, 4, 5}
 
-	for _, value := range array {
-		fmt.Println(value)
+	for indice, value := range array {
+		fmt.Println(indice, value)
 	}
 
 	fmt.Printf("%T\n", array)
