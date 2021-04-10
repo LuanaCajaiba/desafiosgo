39e630f79fdb25e1d5d83fdbb3f52ebd7f4c51ee
LuanaCajaiba <luana_cajaiba1@hotmail.com>
2021-04-10 17:10:42 -0300

    Atualização

---
+++ agrupamentodados/map-range.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	qualquercoisa := map[int]string{
+		123: "muito legal",
+		198: "não é legal",
+		983: "massa",
+		846: "Incrível",
+	}
+
+	fmt.Println(qualquercoisa)
+}
