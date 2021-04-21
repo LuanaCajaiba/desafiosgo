e3c30a741cc865eb9c1735b40ea108370d439841
LuanaCajaiba <luana_cajaiba1@hotmail.com>
2021-04-21 13:17:18 -0300

    update

---
+++ agrupamentodados/contando.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	t := ("Vou aproveitar esse espaço para vender meu peixe! Portanto, falarei da minha trajetória até o momento em que estou agora prestando meu currículo para essa vaga. (Até pra ajudar vocês que estão avaliando, rs). O meu primeiro contato com vendas começa na infância. Decidi que tinha muitos DVDs de desenho animado e resolvi colocar à venda por R$1,00 num valor que na época poderia me render muitos  “negos bom” (doce de banana muito gostoso, recomendo). O segundo momento")
+	//strconv.Itoa
+	/*fmt.Println("Tamanho matematicamente calculado:", math.Floor(math.Log10(math.Abs((57890))))+1)
+	for i := 0; i < len(t); i++ {
+		fmt.Printf("%c\n", t[i])
+	}*/
+	fmt.Println(len(t))
+}
