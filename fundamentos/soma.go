ad74a0aa193a66733e4e8645d81ea2ca16516385
LuanaCajaiba <luana_cajaiba1@hotmail.com>
2021-05-07 21:09:47 -0300

    update

---
+++ fundamentos/soma.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	i := 1
+	x, resultado int 
+	for i <= 50 {
+		var hora int
+		fmt.Println("Ponha os valores aqui: ")
+		fmt.Scanln(&hora)
+
+		resultado = hora * 5, 33
+		fmt.Println(resultado)
+		//x = resultado
+		x + resultado = resultado
+		fmt.Println(resultado)
+
+
+	}
+
+}
